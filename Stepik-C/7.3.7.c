bb01b86648c232e6a62c21f78acae19969bdbaca
Mike <mike.matveev7@gmail.com>
2025-06-11 23:19:13 +0300

    циклы закончены!

---
+++ Stepik-C/7.3.7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void){
+int a,b = 0;
+while(1){
+while (scanf("%d", &a) && a != 0);
+while (scanf("%d", &a) && a != 0)
+b += a;
+while (scanf("%d", &a) && a != -9999);
+break;}
+printf("%d",b);
+return 0;
+}
